ba775a2ac9651abdbf58bb86c680a3db5e6bf4e3
Kaisei Tsujimoto <katsujim@microsoft.com>
2025-06-19 11:51:46 +0900

    プロジェクト ファイルを追加します。

---
+++ InitViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
+
+namespace WpfChartDemo
+{
+    public class InitViewModel
+    {
+        public ISeries[] Series { get; set; } = [
+        new ColumnSeries<int>(3, 4, 2),
+        new ColumnSeries<int>(4, 2, 6),
+        new ColumnSeries<double, DiamondGeometry>(4, 3, 4)
+    ];
+    }
+}
